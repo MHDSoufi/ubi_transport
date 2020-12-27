27d6fa0b6f4bbf45c97829d941b703a319265dea
soufi mehdi <mehdisoufi1991@gmail.com>
2020-12-27 09:54:38 +0100

    BDD configurer et entities creer

---
+++ src/Entity/Note.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NoteRepository::class)
+ */
+class Note
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $matiere;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $valeur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): self
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getValeur(): ?float
+    {
+        return $this->valeur;
+    }
+
+    public function setValeur(float $valeur): self
+    {
+        $this->valeur = $valeur;
+
+        return $this;
+    }
+}
